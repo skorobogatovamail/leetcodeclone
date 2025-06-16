c42dca09e8ec002dea2ddd95a1fa8b8775eb3f4c
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-06-16 16:23:58 +0300

    add logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,15 @@ import Logo from "./Logo";
 import { Button } from "./ui/button";
 import { useAppDispatch } from "@/lib/hooks";
 import { AuthModalState, openAuthModal } from "@/lib/features/authModalSlice";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/firebase";
-import { UserCircle2 } from "lucide-react";
+import { LogOut, UserCircle2 } from "lucide-react";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
   const [user] = useAuthState(auth);
+  const [signOut, loading, error] = useSignOut(auth);
 
   const dispatch = useAppDispatch();
 
@@ -20,17 +21,33 @@ const Navbar: React.FC<NavbarProps> = () => {
     dispatch(openAuthModal(type));
   };
 
+  const handleLogout = async () => {
+    try {
+      const success = await signOut();
+      if (!success) throw new Error("Logout failed")
+    } catch (error) {
+      alert(error)
+    }
+
+  };
+
   return (
     <div className="flex items-center justify-between px-15 sm:px-20 py-5 border-b">
       <Logo />
 
       {user ? (
-        <div className="relative group">
-          <UserCircle2 className="h-8 w-8" />
-          <p className="absolute scale-0 group-hover:scale-100 bg-neutral-200 top-10 -translate-x-1/2 left-1/2 p-2 rounded-lg text-orange-500 transition-all duration-200 ease-out text-sm">
-            {user?.email}
-          </p>
+        <div className="flex items-center justify-between gap-2">
+          <div className="relative group">
+            <UserCircle2 className="h-8 w-8" />
+            <p className="absolute scale-0 group-hover:scale-100 bg-neutral-200 top-10 -translate-x-1/2 left-1/2 p-2 rounded-lg text-orange-500 transition-all duration-200 ease-out text-sm">
+              {user?.email}
+            </p>
+          </div>
+          <Button variant="outline" onClick={handleLogout}>
+            <LogOut />
+          </Button>
         </div>
+
       ) : (
         <div className="flex items-center justify-between gap-2">
           <Button variant="outline" onClick={() => handleClick("login")}>
@@ -38,8 +55,9 @@ const Navbar: React.FC<NavbarProps> = () => {
           </Button>
           <Button onClick={() => handleClick("register")}>Sign Up</Button>
         </div>
-      )}
-    </div>
+      )
+      }
+    </div >
   );
 };
 export default Navbar;
