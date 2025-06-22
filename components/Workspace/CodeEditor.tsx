a50816d46e2fadd2512cdf7a4f5b991af52f468d
skorobogatovamail <skorobogatovamail@gmail.com>
2025-06-22 14:09:24 +0300

    add PreferenceNav

--- components/Workspace/CodeEditor.tsx
+++ components/Workspace/CodeEditor.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PreferenceNav from "./PreferenceNav";
 
 type CodeEditorProps = {};
 
 const CodeEditor: React.FC<CodeEditorProps> = () => {
   return (
     <div className="px-4 sm:px-10 rounded h-full border-2 py-4">
-      Have a good coding
+      <PreferenceNav />
     </div>
   );
 };
