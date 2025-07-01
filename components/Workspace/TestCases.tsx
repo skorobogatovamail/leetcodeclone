c01320e8b208283b096ddc62fed4946ffe62e42a
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-07-01 14:18:23 +0300

    update test cases

--- components/Workspace/TestCases.tsx
+++ components/Workspace/TestCases.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { Problem } from "@/data/types/problem";
 
@@ -6,6 +8,8 @@ interface TestCasesProps {
   testCases: Problem['examples']
 }
 const TestCases: React.FC<TestCasesProps> = ({ testCases }) => {
+  const [activeTestCase, setActiveTestCase] = useState<number>(0)
+
   return (
     <div className="px-6">
       <div>
@@ -16,25 +20,25 @@ const TestCases: React.FC<TestCasesProps> = ({ testCases }) => {
       </div>
 
       <div className="flex mt-3 gap-2">
-        {testCases.map(el => (
-          <Button key={el.id} variant="secondary">Case {el.id + 1}</Button>
+        {testCases.map((el, idx) => (
+          <Button
+            key={el.id}
+            variant={activeTestCase === idx ? "secondary" : "ghost"}
+            onClick={() => setActiveTestCase(idx)}>
+            Case {el.id + 1}
+          </Button>
         ))}
       </div>
 
       <div className=" my-4">
-        {testCases.map(el => (
-          <React.Fragment key={el.id}>
-            <p className="text-sm font-medium mt-4 ">Input:</p>
-            <div className="cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent mt-2 text-sm bg-neutral-200">
-              {el.inputText}
-            </div>
-            <p className="text-sm font-medium mt-4 ">Output:</p>
-            <div className=" cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent  mt-2 text-sm  bg-neutral-200">
-              {el.outputText}
-            </div>
-          </React.Fragment>
-
-        ))}
+        <p className="text-sm font-medium mt-4 ">Input:</p>
+        <div className="cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent mt-2 text-sm bg-neutral-200">
+          {testCases[activeTestCase].inputText}
+        </div>
+        <p className="text-sm font-medium mt-4 ">Output:</p>
+        <div className=" cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent  mt-2 text-sm  bg-neutral-200">
+          {testCases[activeTestCase].outputText}
+        </div>
       </div>
     </div>
   );
