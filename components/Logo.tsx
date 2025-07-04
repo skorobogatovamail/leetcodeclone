9e78385f1e1aa58dcdf89298a9b7c2f1d28e6a86
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-07-04 18:43:31 +0300

    format code

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,15 +4,13 @@ import React from "react";
 
 const Logo: React.FC = () => {
   return (
-    <div className="">
-      <Link
-        href="/"
-        className="text-xl font-bold flex justify-between gap-2 items-center"
-      >
-        <GraduationCap />
-        <span>LeetcodeClone</span>
-      </Link>
-    </div>
+    <Link
+      href="/"
+      className="text-xl font-bold flex justify-between gap-2 items-center"
+    >
+      <GraduationCap />
+      <span>LeetcodeClone</span>
+    </Link>
   );
 };
 export default Logo;
